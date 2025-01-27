1545f7a3141225ced38aefe2de4810484cce2c88
acpereira <andrepereira2611@gmail.com>
2025-01-27 18:10:34 -0300

    :feat adicionando lógica de registro de chamados

--- abrir_chamado.php
+++ abrir_chamado.php
@@ -44,15 +44,15 @@
               <div class="row">
                 <div class="col">
                   
-                  <form>
+                  <form action="registra_chamado.php" method="POST">
                     <div class="form-group">
                       <label>Título</label>
-                      <input type="text" class="form-control" placeholder="Título">
+                      <input type="text" class="form-control" placeholder="Título" name="titulo">
                     </div>
                     
                     <div class="form-group">
                       <label>Categoria</label>
-                      <select class="form-control">
+                      <select class="form-control" name="categoria">
                         <option>Criação Usuário</option>
                         <option>Impressora</option>
                         <option>Hardware</option>
@@ -63,7 +63,7 @@
                     
                     <div class="form-group">
                       <label>Descrição</label>
-                      <textarea class="form-control" rows="3"></textarea>
+                      <textarea class="form-control" rows="3" name="descricao"></textarea>
                     </div>
 
                     <div class="row mt-5">
--- index.php
+++ index.php
@@ -51,7 +51,7 @@
                 <?php } ?>
                 <?php if(isset($_GET['login']) && $_GET['login'] == 'erro2') { ?>
                   <div class="text-warning">
-                    Faça o login antes de acessar o sistema                     
+                    <p class="mt-1">Faça o login antes de acessar o sistema<p>                     
                   </div>                    
                 <?php } ?>
                 <button class="btn btn-lg btn-info btn-block" type="submit">Entrar</button>
