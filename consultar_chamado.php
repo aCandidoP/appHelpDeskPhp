fa1529ef1067923345fbd1304d2ae64a0964e2c9
acpereira <andrepereira2611@gmail.com>
2025-01-28 09:01:31 -0300

    :feat adicionando funcionalidade de consulta de chamados

--- consultar_chamado.php
+++ consultar_chamado.php
@@ -2,6 +2,22 @@
   require_once("validador_acesso.php")
 ?>
 
+<?php 
+
+$chamados = array();
+$arquivo = "/var/www/html/appHelpDesk/arquivo.hd";
+fopen($arquivo, "r");
+
+while(!feof($arquivo)) {
+  $registro = fgets($arquivo);
+  $chamados[] = $registro;
+
+}
+
+fclose($arquivo);
+
+?>
+
 <html>
   <head>
     <meta charset="utf-8" />
@@ -43,23 +59,28 @@
             
             <div class="card-body">
               
-              <div class="card mb-3 bg-light">
-                <div class="card-body">
-                  <h5 class="card-title">Título do chamado...</h5>
-                  <h6 class="card-subtitle mb-2 text-muted">Categoria</h6>
-                  <p class="card-text">Descrição do chamado...</p>
+              <?php 
+              
+                foreach($chamados as $chamado) {
+              
+              ?>
 
+              <?php 
+              
+              $chamado_dados = explode('#', $chamado);
+
+              ?>
+
+                <div class="card mb-3 bg-light">
+                  <div class="card-body">
+                    <h5 class="card-title"><?php $chamado_dados[0] ?></h5>
+                    <h6 class="card-subtitle mb-2 text-muted"><?php $chamado_dados[1] ?></h6>
+                    <p class="card-text"><?php $chamado_dados[2] ?></p>
+
+                  </div>
                 </div>
-              </div>
 
-              <div class="card mb-3 bg-light">
-                <div class="card-body">
-                  <h5 class="card-title">Título do chamado...</h5>
-                  <h6 class="card-subtitle mb-2 text-muted">Categoria</h6>
-                  <p class="card-text">Descrição do chamado...</p>
-
-                </div>
-              </div>
+              <?php } ?>
 
               <div class="row mt-5">
                 <div class="col-6">
